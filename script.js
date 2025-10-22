7a5a6c568e085bfdeaf7b3f4dea99d7ccf64a420
maksatai190803-debug <maksatai190803@gmail.com>
2025-10-22 21:59:59 +0600

    HW_№2

--- script.js
+++ script.js
@@ -30,113 +30,137 @@
 // var sum = (input - percent);
 // console.log('Сумма со скидкой: ' + sum + '$');
 
-const orders = [
- {
-   id: 1,
-   customer: "Jhon",
-   items: [
-     {
-       name: "Ноутбук",
-       price: 50000,
-       quantity: 1,
-     },
-     {
-       name: "Мышь",
-       price: 1500,
-       quantity: 2,
-     },
-   ],
- },
- {
-   id: 2,
-   customer: "Alan",
-   items: [
-     {
-       name: "Телефон",
-       price: 30000,
-       quantity: 1,
-     },
-     {
-       name: "Чехол",
-       price: 1000,
-       quantity: 3,
-     },
-   ],
- },
- {
-   id: 3,
-   customer: "Jane",
-   items: [
-     {
-       name: "Клавиатура",
-       price: 2500,
-       quantity: 1,
-     },
-     {
-       name: "Монитор",
-       price: 12000,
-       quantity: 1,
-     },
-   ],
- },
-]; 
+// const orders = [
+//  {
+//    id: 1,
+//    customer: "Jhon",
+//    items: [
+//      {
+//        name: "Ноутбук",
+//        price: 50000,
+//        quantity: 1,
+//      },
+//      {
+//        name: "Мышь",
+//        price: 1500,
+//        quantity: 2,
+//      },
+//    ],
+//  },
+//  {
+//    id: 2,
+//    customer: "Alan",
+//    items: [
+//      {
+//        name: "Телефон",
+//        price: 30000,
+//        quantity: 1,
+//      },
+//      {
+//        name: "Чехол",
+//        price: 1000,
+//        quantity: 3,
+//      },
+//    ],
+//  },
+//  {
+//    id: 3,
+//    customer: "Jane",
+//    items: [
+//      {
+//        name: "Клавиатура",
+//        price: 2500,
+//        quantity: 1,
+//      },
+//      {
+//        name: "Монитор",
+//        price: 12000,
+//        quantity: 1,
+//      },
+//    ],
+//  },
+// ]; 
 
-console.log("Сумма каждого заказа:");
-for (var i = 0; i < orders.length; i++) {
-  var order = orders[i];         
-  var total = 0;                 
+// console.log("Сумма каждого заказа:");
+// for (var i = 0; i < orders.length; i++) {
+//   var order = orders[i];         
+//   var total = 0;                 
   
-  for (var j = 0; j < order.items.length; j++) {
-    var item = order.items[j];
-    total += item.price * item.quantity;
-  }
+//   for (var j = 0; j < order.items.length; j++) {
+//     var item = order.items[j];
+//     total += item.price * item.quantity;
+//   }
   
-  order.total = total;
-  console.log(`${order.customer} — ${total} руб.`);
-}
+//   order.total = total;
+//   console.log(`${order.customer} — ${total} руб.`);
+// }
 
-var totalIncome = 0;
-for (var i = 0; i < orders.length; i++) {
-  totalIncome += orders[i].total;
-}
-console.log(`\nОбщий доход магазина: ${totalIncome} руб.`);
+// var totalIncome = 0;
+// for (var i = 0; i < orders.length; i++) {
+//   totalIncome += orders[i].total;
+// }
+// console.log(`\nОбщий доход магазина: ${totalIncome} руб.`);
 
-var maxOrder = orders[0];
-for (var i = 1; i < orders.length; i++) {
-  if (orders[i].total > maxOrder.total) {
-    maxOrder = orders[i];
-  }
-}
-console.log(`\nСамый дорогой заказ сделал(а): ${maxOrder.customer} (${maxOrder.total} руб.)`);
+// var maxOrder = orders[0];
+// for (var i = 1; i < orders.length; i++) {
+//   if (orders[i].total > maxOrder.total) {
+//     maxOrder = orders[i];
+//   }
+// }
+// console.log(`\nСамый дорогой заказ сделал(а): ${maxOrder.customer} (${maxOrder.total} руб.)`);
 
-var products = {};
+// var products = {};
 
-for (var i = 0; i < orders.length; i++) {
-  for (var j = 0; j < orders[i].items.length; j++) {
-    var item = orders[i].items[j];
+// for (var i = 0; i < orders.length; i++) {
+//   for (var j = 0; j < orders[i].items.length; j++) {
+//     var item = orders[i].items[j];
     
-    if (products[item.name]) {
-      products[item.name] += item.quantity;
-    } else {
-      products[item.name] = item.quantity;
+//     if (products[item.name]) {
+//       products[item.name] += item.quantity;
+//     } else {
+//       products[item.name] = item.quantity;
+//     }
+//   }
+// }
+
+// console.log("\nСписок всех проданных товаров:");
+// for (var name in products) {
+//   console.log(`${name}: ${products[name]} шт.`);
+// }
+
+
+// var popularName = null;
+// var maxCount = 0;
+
+// for (var name in products) {
+//   if (products[name] > maxCount) {
+//     maxCount = products[name];
+//     popularName = name;
+//   }
+// }
+
+// console.log(`\nСамый популярный товар: ${popularName} (${maxCount} шт.)`);
+
+
+
+// создаем функцию, которая ищет самое большое число в массиве
+function findMax(arr) {
+  var max = arr[0]; 
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]; 
     }
   }
+
+  return max; 
 }
 
-console.log("\nСписок всех проданных товаров:");
-for (var name in products) {
-  console.log(`${name}: ${products[name]} шт.`);
-}
+var numbers1 = [5, 8, 2, 10, 3];
+var numbers2 = [15, 25, 5, 30, 12];
+var numbers3 = [100, 50, 70, 20, 10];
 
+console.log("Максимальное значение в первом массиве:", findMax(numbers1));
+console.log("Максимальное значение во втором массиве:", findMax(numbers2));
+console.log("Максимальное значение в третьем массиве:", findMax(numbers3));
 
-var popularName = null;
-var maxCount = 0;
-
-for (var name in products) {
-  if (products[name] > maxCount) {
-    maxCount = products[name];
-    popularName = name;
-  }
-}
-
-console.log(`\nСамый популярный товар: ${popularName} (${maxCount} шт.)`);
